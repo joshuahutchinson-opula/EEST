1047155df31328926510c134a35da02020af4386
joshuahutchinson-opula <joshua.ls.hutchinson@gmail.com>
2026-07-20 13:38:37 -0500

    Add allowedHosts to Vite preview configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+   preview: {
+    allowedHosts: ['end-to-end-sales-tool-production.up.railway.app']
   plugins: [react()],
 })
